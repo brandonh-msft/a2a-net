7df7946403e756b850c60a8674a5100ba56c6fd5
Brandon🌩️H <brandonh-msft@users.noreply.github.com>
2025-05-07 14:53:15 -0700

    allowing for query strings in the card request

--- src/a2a-net.Client/Extensions/HttpClientExtensions.cs
+++ src/a2a-net.Client/Extensions/HttpClientExtensions.cs
@@ -33,7 +33,14 @@ public static class HttpClientExtensions
         try
         {
             var path = ".well-known/agent.json";
-            var endpoint = request.Address == null ? new Uri($"/{path}", UriKind.Relative) : new Uri(request.Address, path);
+            var builder = new UriBuilder(request.Address?.ToString() ?? $"/{path}");
+            if (request.Address is not null)
+            {
+                builder.Query = request.Address.Query;
+                builder.Path = builder.Path.TrimEnd('/') + $"/{path}";
+            }
+
+            var endpoint = builder.Uri;
             var agentCard = await httpClient.GetFromJsonAsync<AgentCard>(endpoint, cancellationToken).ConfigureAwait(false);
             return new()
             {
@@ -41,7 +48,7 @@ public static class HttpClientExtensions
                 Agents = agentCard == null ? [] : [agentCard]
             };
         }
-        catch (HttpRequestException ex) when(ex.StatusCode == HttpStatusCode.NotFound)
+        catch (HttpRequestException ex) when (ex.StatusCode == HttpStatusCode.NotFound)
         {
             var path = ".well-known/agents.json";
             var endpoint = request.Address == null ? new Uri($"/{path}", UriKind.Relative) : new Uri(request.Address, path);
